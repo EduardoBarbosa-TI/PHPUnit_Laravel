a484b87c0ed480f70232df02f78936884fb60cc1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-02-17 23:47:33 -0300

    Utilizando exeções do PHPUnit e realizando testes

--- laravel_phpunit/app/Models/Leilao.php
+++ laravel_phpunit/app/Models/Leilao.php
@@ -8,28 +8,31 @@ class Leilao
     private $lances;
     /** @var string */
     private $descricao;
+    private $finalizado;
 
     public function __construct(string $descricao)
     {
         $this->descricao = $descricao;
         $this->lances = [];
+        $this->finalizado = false;
     }
 
     public function recebeLance(Lance $lance)
     {
-        if(!empty($this->lances) && $this->ehDoUltimousuario($lance)){
-            return;
+        if(!empty($this->lances) && $this->ehDoUltimousuario($lance))
+        {
+            throw new \DomainException('Usuário não pode propor 2 lances consecutivos');
         }
 
         $totalLancesUsuario = $this
             ->quantidaDeLancesPorUsuario($lance->getUsuario());
 
-        if ($totalLancesUsuario >= 5) {
-            return;
+        if ($totalLancesUsuario >= 5) 
+        {
+            throw new \DomainException('Usuário não pode porpor mais de 5 lances por leilão');
         }
         $this->lances[] = $lance;
     }
-
     /**
      * @return Lance[]
      */
@@ -37,7 +40,14 @@ class Leilao
     {
         return $this->lances;
     }
-
+    public function finaliza()
+    {
+        $this->finalizado = true;
+    }
+    public function estaFinalizado(): bool
+    {
+        return $this->finalizado;
+    }
     private function quantidaDeLancesPorUsuario(Usuario $usuario): int 
     {
         $totalLancesUsuario = array_reduce($this->lances, function($totalAcumulado, Lance $lanceAtual) use ($usuario){
